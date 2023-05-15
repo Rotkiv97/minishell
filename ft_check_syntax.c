d198048c33717d3a44381237a85b2c9bc650adea
Damiano Colucci <dcolucci@c2r3p6.42firenze.it>
2023-05-15 15:43:13 +0200

    syntax_check

---
+++ ft_check_syntax.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_check_syntax.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcolucci <dcolucci@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/15 14:56:10 by dcolucci          #+#    #+#             */
+/*   Updated: 2023/05/15 15:40:33 by dcolucci         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	ft_count_redirection(char **sub_cmd, char *redir)
+{
+	int		x;
+	int		count;
+
+	x = 0;
+	count = 0;
+	while (sub_cmd[x])
+	{
+		if (sub_cmd[x][0] == redir && (int)ft_strlen(sub_cmd[x] == 1))
+			count++;
+		else if (sub_cmd[x][0] != redir && in_set(sub_cmd[x][0], "<>") \
+			&& (int)ft_strlen(sub_cmd[x]) == 1)
+			return (INT_MAX);
+		x++;
+	}
+	return (count);
+}
+
+void	ft_check_syntax(char **sub_cmd)
+{
+	int		x;
+	int		count;
+	char	*error;
+
+	x = 0;
+	count = 0;
+	error = "\033[7m\033[33mSytax error using < >\n\033[0m";
+	if (!sub_cmd)
+		return ;
+	while (sub_cmd[x])
+	{
+		if (in_set(sub_cmd[x][0], "<>"))
+		{
+			count = ft_count_redirection(&sub_cmd[x + 1], sub_cmd[x][0]);
+			if (count > 2 || !sub_cmd[x + 1])
+				ft_quit(error, -1);
+		}
+	}
+}
--- ft_quit.c
+++ ft_quit.c
@@ -1,42 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_create_cmds.c                                   :+:      :+:    :+:   */
+/*   ft_quit.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcolucci <dcolucci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/14 17:58:07 by vguidoni          #+#    #+#             */
-/*   Updated: 2023/05/15 15:41:18 by dcolucci         ###   ########.fr       */
+/*   Created: 2023/05/15 15:20:17 by dcolucci          #+#    #+#             */
+/*   Updated: 2023/05/15 15:21:55 by dcolucci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-t_list	*ft_new_cmd(char **full_cmds, int x, int y)
+void	ft_quit(char *error, int quit)
 {
-	
-}
-
-t_list	*ft_create_cmds(char **full_cmds)
-{
-	t_list	*cmds;
-	t_list	*new;
-	int		x;
-	int		y;
-
-	x = 0;
-	y = 0;
-	if (full_cmds == NULL)
-		return (NULL);
-	while(full_cmds[x])
-	{
-		if(full_cmds[x][0] == '|')
-		{
-        	new = ft_new_cmd(full_cmds, x, y);
-        	y = x;
-		}
-		x++;
-	}
-	new = ft_new_cmd(fulls_cmds, x, y);
-	return (cmds)
+	printf("%s", error);
+	exit(quit);
 }
